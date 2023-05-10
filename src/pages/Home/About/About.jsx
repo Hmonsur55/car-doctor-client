e5490ae5e0a81ebab3f027b383111eb193cfb21d
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-10 12:30:18 +0600

    add about section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -1,10 +1,39 @@
-
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
 
 const About = () => {
     return (
-        <div>
-            
+      <div className="hero min-h-screen">
+        <div className=" flex gap-4 lg:flex-row ">
+          <div className="relative w-2/3">
+            <img src={person} className="w-2/3 rounded-lg shadow-2xl" />
+            <img
+              src={parts}
+              className="w-1/2 rounded-lg shadow-2xl absolute right-5 top-1/2 border-8 border-white"
+            />
+          </div>
+          <div className="w-1/2">
+            <h4 className="text-2xl font-bold text-orange-500">About Us</h4>
+            <h1 className="text-5xl font-bold">
+              We are qualified & of experience in this field
+            </h1>
+            <p className="py-4">
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable.
+            </p>
+            <p className="py-4">
+              the majority have suffered alteration in some form, by injected
+              humour, or randomised words which don't look even slightly
+              believable.
+            </p>
+            <button className="btn btn-primary bg-orange-500 border-none">
+              Get More Info
+            </button>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../../components/Banner/Banner";
 import NavBar from "../../Shared/NavBar/NavBar";
+import About from "../About/About";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <NavBar></NavBar>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
