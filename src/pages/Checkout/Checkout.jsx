7af6b6aa9bc994ed7284bd78a2f9b14bf3e7897b
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-13 22:50:05 +0600

    replace static service data to api

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,11 @@
+
+
+const Checkout = () => {
+    return (
+        <div>
+            <h1>check out</h1>
+        </div>
+    );
+};
+
+export default Checkout;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
 import Main from "../layout/Main";
+import Checkout from "../pages/Checkout/Checkout";
 import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: '/register',
        element: <Register></Register> 
+      },
+      {
+        path: '/checkout/:id',
+       element: <Checkout></Checkout>, 
       }
     ],
   },
